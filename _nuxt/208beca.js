aa609e5e0e086833cc9db4259b88c64115f08f5a
tandpfun <tandpfun@users.noreply.github.com>
2021-12-23 19:05:54 +0000

    deploy: e92c4dbe55f33e02af8ec22fca8903eacd30b549

---
+++ _nuxt/208beca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{264:function(t,e,n){"use strict";n.r(e);var l=n(50),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("img",{staticClass:"bottom-0 left-0 absolute w-2/5 pointer-events-none",attrs:{src:"/hills.svg"}})}),[],!1,null,null,null);e.default=component.exports}}]);
